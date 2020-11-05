8f5bf2796511ac324570134006dbaf911eccc728
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2020-11-05 20:00:40 +0900

    Active Storageの導入

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -25,6 +25,6 @@ class RoomsController < ApplicationController
   private
 
   def room_params
-    params.require(:room).permit(:name, user_id: [])
+    params.require(:room).permit(:name, user_ids: [])
   end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ApplicationRecord
-  validates :room, presence: true
+  validates :name, presence: true
   
   has_many :room_users
   has_many :users, through: :room_users, dependent: :destroy
